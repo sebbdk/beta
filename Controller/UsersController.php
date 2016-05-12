9c0108f6aed78804a7863544ddea8a6e9e4eb07d
kasper <sebb@sebb.dk>
2016-05-12 19:04:00 +0200

    fixed slug issue

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -3,17 +3,21 @@
  * @Author: sebb
  * @Date:   2015-03-16 17:51:01
  * @Last Modified by:   kasper
- * @Last Modified time: 2016-05-12 18:55:24
+ * @Last Modified time: 2016-05-12 19:03:20
  */
 App::uses('AppController', 'Controller');
 
 class UsersController extends AppController {
 
+	private $slug = "";
+
 	public function add($gameSlug = "no-game") {
 		$this->set('gameSlug', $this->params['slug']);
 
+		Configure::write('slug', $gameSlug);
+
 		$this->Crud->on('afterSave', function(CakeEvent $event) {
-			$this->redirect('/users/index');
+			$this->redirect('/users/index/'. $this->request->params['slug']);
 		});
 
 		if($this->request->is('post')) {
